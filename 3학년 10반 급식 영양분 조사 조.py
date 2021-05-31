e0ecb1b7619a40c1aaba9b88bfe96fac6ee1e4e2
jomingyeong <84947383+jomingyeong@users.noreply.github.com>
2021-05-31 14:52:29 +0900

    Add files via upload

---
+++ 3학년 10반 급식 영양분 조사 조.py
@@ -0,0 +1,25 @@
+food1 = input("음식이름을 입력하시오:")
+food2 = input("음식이름을 입력하시오:")
+food3 = input("음식이름을 입력하시오:")
+food4 = input("음식이름을 입력하시오:")
+food5 = input("음식이름을 입력하시오:")
+food6 = input("음식이름을 입력하시오:")
+
+
+밥 =
+국 =
+잔반 =
+
+
+if food1 == 밥[0]:
+    print("밥영양분~~")
+if food2 == 밥[0]:
+    print("밥영양분~~")
+if food3 == 밥[0]:
+    print("밥영양분~~")
+if food4 == 밥[0]:
+    print("밥영양분~~")
+if food5 == 밥[0]:
+    print("밥영양분~~")
+if food6 == 밥[0]:
+    print("밥영양분~~")
